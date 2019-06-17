4e82b313cfa552953694badaf744c74e76b2f76a
Yogesh kumar <92kumaryogi@gmail.com>
2019-06-17 15:22:33 +0530

    Initial commit

---
+++ HttpHandlerDemoXss/HttpHandlerClass.vb
@@ -0,0 +1,13 @@
+﻿Public Class HttpHandlerClass
+    Implements IHttpHandler
+
+    Public ReadOnly Property IsReusable As Boolean Implements IHttpHandler.IsReusable
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Sub ProcessRequest(context As HttpContext) Implements IHttpHandler.ProcessRequest
+        context.Response.WriteFile(System.IO.Path.GetFileName(context.Server.UrlEncode(context.Request.Path)))
+    End Sub
+End Class
